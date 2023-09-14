efa416021bfced431e33171e96c60db43c3c1f60
Vambush <48507154+Vambush@users.noreply.github.com>
2023-09-13 23:15:28 -0400

    Lab 13 Homework

---
+++ Programming Exercises/Lab13.py
@@ -0,0 +1,18 @@
+#Name: Vansh Bataviya
+#Email: vansh.bataviya94@myhunter.cuny.edu
+#Date: 09/13/2023
+#This program draws a cornflower blue pentagon using the turtle module.
+
+import turtle
+
+turt = turtle.Turtle()
+turt.shape("turtle")
+turt.color("cornflowerblue")
+
+for i in range(5):
+    turt.forward(100)
+    turt.stamp()
+    turt.left(72)
+
+
+turtle.Screen().exitonclick()
