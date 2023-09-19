6f1323c91f33bd47b381ad3b7fd6a144c3e2d713
Vambush <48507154+Vambush@users.noreply.github.com>
2023-09-18 21:42:51 -0400

    Added Lab16

---
+++ Programming Exercises/Lab16.py
@@ -0,0 +1,15 @@
+#Name: Vansh Bataviya
+#Email: vansh.bataviya94@myhunter.cuny.edu
+#Date: 09/05/2023
+#This program asks the user for 5 whole (integer) numbers. For each number, turn the turtle left the degrees entered and then the turtle should move forward 100..
+
+import turtle
+
+turt = turtle.Turtle()
+
+for i in range(5):
+    val = int(input("Enter a number:"))
+    turt.left(val)
+    turt.forward(100)
+
+turtle.Screen().exitonclick()
