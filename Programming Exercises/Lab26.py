10f2c1ca92f373d18f0d44da362027574bdad7bd
Vansh Bataviya <bataviyavansh@gmail.com>
2023-11-19 23:45:16 -0500

    Added Recent Labs and fixed Code Locations

--- Programming Exercises/Lab26.py
+++
@@ -1,7 +0,0 @@
-#Name: Vansh Bataviya
-#Email: vansh.bataviya94@myhunter.cuny.edu
-#Date: 10/05/2023
-#Build a circuit that has the same behavior as a nand gate (i.e. for the same inputs, gives identical output) using only and, or, and not gates.
-#It doesn't run bozo
-
-out = not(in1 or in2)
